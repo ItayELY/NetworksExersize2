c6e3a367a1312083cb83a05c000e1279bf348c85
yonadavGit <yonadav.le@gmail.com>
2021-11-30 15:54:37 -0800

    creating client which can send a file, and server which can read it and put it into folder

--- Client.py
+++ Client.py
@@ -30,47 +30,90 @@ def on_moved(event):
     print(f"{event.src_path} has been moved to {event.dest_path}")
 
 
+#####
+def send_file(file_name, socket):
+    final_file = os.path.join(os.getcwd(), file_name)
+    file_size = os.path.getsize(final_file)
+    #socket.send(file_name.encode())
+    #socket.send(str(file_size).encode())
+    send_word(file_name, socket)
+    send_word(file_size,socket)
+    # Opening file and sending data.
+    with open(file_name, "rb") as file:
+        c = 0
+        # Starting the time capture.
+        start_time = time.time()
+        # Running loop while c != file_size.
+        while c < file_size:
+            data = file.read(1024)
+            if not (data):
+                break
+            socket.sendall(data)
+            c += len(data)
+
+        # Ending the time capture.
+        end_time = time.time()
+    print("File Transfer client Complete.Total time: ", end_time - start_time)
+
+
+def send_word(word, socket):
+    c = 0
+    word = str(word) + '~'
+    # Starting the time capture.
+    # Running loop while c != file_size.
+    while c < len(word):
+        data = word[c]
+        if not (data):
+            break
+        socket.sendall(data.encode())
+        c += len(data)
+
+
 if __name__ == "__main__":
     #open socket:
-    print("1")
     skClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("2")
     skClient.connect(('127.0.0.1', 12345))
-    print("1")
+    send_file("a.txt",skClient)
 
-    #going through each file in given folder
-    for file_name in os.listdir(folderPath):
-        #opening the file and sending to the server 1024b at a time:
-        file = open(file_name, "wb")
-        skClient.send(file)
-        sRead = file.read(1024)
-        while sRead:
-            skClient.send(sRead)
-            sRead = file.read(1024)
-        skClient.send("endfile")
 
-    patterns = ["*"]
-    ignore_patterns = None
-    ignore_directories = False
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-    my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
 
-    go_recrsive = True
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, folderPath, recursive=go_recrsive)
-
-    my_observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        my_observer.stop()
-        my_observer.join()
+    #
+    # #going through each file in given folder
+    # for file_name in os.listdir(folderPath):
+    #     #opening the file and sending to the server 1024b at a time:
+    #     file = open(file_name, "wb")
+    #     skClient.send(file)
+    #     sRead = file.read(1024)
+    #     while sRead:
+    #         skClient.send(sRead)
+    #         sRead = file.read(1024)
+    #     skClient.send("endfile")
+    #
+    #
+    # #Watchdog
+    # patterns = ["*"]
+    # ignore_patterns = None
+    # ignore_directories = False
+    # case_sensitive = True
+    # my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    #
+    # my_event_handler.on_created = on_created
+    # my_event_handler.on_deleted = on_deleted
+    # my_event_handler.on_modified = on_modified
+    # my_event_handler.on_moved = on_moved
+    #
+    # go_recrsive = True
+    # my_observer = Observer()
+    # my_observer.schedule(my_event_handler, folderPath, recursive=go_recrsive)
+    #
+    # my_observer.start()
+    # try:
+    #     while True:
+    #         time.sleep(1)
+    # except KeyboardInterrupt:
+    #     my_observer.stop()
+    #     my_observer.join()
 
 
 
--- Server.py
+++ Server.py
@@ -1,29 +1,83 @@
 import socket
 import os
+import time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
 
+
+def receive_file(file_name, file_size, directory, socket):
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, directory, file_name)
+    print(final_directory)
+    with open(final_directory, "wb") as file:
+        print("file size is " + str(file_size))
+        print("file names is " + file_name)
+        c = 0
+        # Starting the time capture.
+        start_time = time.time()
+
+        # Running the loop while file is received.
+        while c < int(file_size):
+            data = socket.recv(1024)
+            if not (data):
+                break
+            file.write(data)
+            c += len(data)
+
+        # Ending the time capture.
+        end_time = time.time()
+
+    print("File transfer Complete.Total time: ", end_time - start_time)
+
+def receive_word(socket):
+    put_word_in_me = ''
+    c = 0
+    while True:
+        data = socket.recv(1).decode()
+        if data == '~':
+            break
+        put_word_in_me = put_word_in_me + data
+        c += len(data)
+    return put_word_in_me
 if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', 12345))
+
     #open clients directory:
     current_directory = os.getcwd()
     final_directory = os.path.join(current_directory, r'clients')
     if not os.path.exists(final_directory):
         os.makedirs(final_directory)
     server.listen(5)
+
     while True:
-        client_socket, client_address = server.accept()
-        print('Connection from: ', client_address)
-        file_name = client_socket.recv(1024)
-        print('Received: ', file_name)
-        #creating new client directory:
-        client_directory = os.path.join(final_directory + file_name, r'client1')
-        sData = client_socket.recv(1024)
-        while sData:
-            fDownloadFile = open(client_directory, "wb")
-            #endfile- special packet indicates file ended.
-            while sData != "endfile":
-                fDownloadFile.write(sData)
-                sData = client_socket.recv(1024)
-        print("Download Completed")
-        break
+        print("HI!")
+        connected_socket, connected_address = server.accept()
+        print('Connection from: ' + str(connected_address))
+        #receiving file details
+
+        #file_name = connected_socket.recv(5).decode()
+        #file_size = connected_socket.recv(3).decode()
+        file_name = receive_word(connected_socket)
+        file_size = receive_word(connected_socket)
+        print("file size is " + file_size)
+        print("file names is " + file_name)
+
+        #writing received file to directory
+        receive_file(file_name, file_size, "clients", connected_socket)
+
+
+        # client_socket, client_address = server.accept()
+        # print('Connection from: ', client_address)
+        # file_name = client_socket.recv(1024)
+        # print('Received: ', file_name)
+        # #creating new client directory:
+        # client_directory = os.path.join(final_directory + file_name, r'client1')
+        # sData = client_socket.recv(1024)
+        # while sData:
+        #     fDownloadFile = open(client_directory, "wb")
+        #     #endfile- special packet indicates file ended.
+        #     while sData != "endfile":
+        #         fDownloadFile.write(sData)
+        #         sData = client_socket.recv(1024)
+        # print("Download Completed")
+        # break
